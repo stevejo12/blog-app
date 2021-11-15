7da424ed3fdfb7f7a9226e9981f5d7e869864ca8
stevejo12 <37164804+stevejo12@users.noreply.github.com>
2021-11-15 16:58:41 +0700

    feat: blog

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,10 +1,99 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import { Container } from '../globalStyles';
+import BlogData from '../data/blog.json';
+
+export const Wrapper = styled(Container)`
+  min-height: 85vh;
+`;
+
+const CardRow = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const Card = styled.div`
+  flex: 33.3%;
+  max-width: 33.3%;
+  padding: 3rem 0.5rem;
+
+  @media screen and (max-width: 960px) {
+    flex: 50%;
+    max-width: 50%;
+  }
+
+  @media screen and (max-width: 600px) {
+    flex: 100%;
+    max-width: 100%;
+  }
+`;
+
+const CardLink = styled(Link)`
+  text-decoration: none;
+  color: #fff;
+`;
+
+const CardImage = styled.img`
+  width: 100%;
+  border-radius: 5px;
+`;
+
+const CardDate = styled.p`
+  font-size: 0.9rem;
+  font-weight: 600;
+  margin-top: 10px;
+  color: #9a9a9a;
+`;
+
+const CardHeading = styled.h1`
+  font-size: 1.5rem;
+  margin-top: 10px;
+
+  &:hover {
+    color: #4b59f7;
+    transition: all 0.3 ease-in-out;
+  }
+`;
+
+const CardDescription = styled.p`
+  display: block;
+  display: -webkit-box;
+  margin-top: 10px;
+  font-size: 1rem;
+  overflow: hidden;
+  line-height: 2rem;
+  max-height: 8rem;
+  text-overflow: ellipsis;
+  -webkit-line-clamp: 3;
+  -webkit-box-orient: vertical;
+`;
 
 const Blog = () => {
   return (
-    <div>
-      This is Blog Page
-    </div>
+    <Wrapper>
+      <Container>
+        <CardRow>
+          {
+            BlogData.map((blogDetail, index) => {
+              return (
+                <Card key={index}>
+                  <CardLink to="/">
+                    <CardImage src={blogDetail.image} alt="" />
+                  </CardLink>
+                  <CardDate>{blogDetail.date}</CardDate>
+                  <CardLink to="/">
+                    <CardHeading>{blogDetail.title}</CardHeading>
+                    <CardDescription>{blogDetail.description}</CardDescription>
+                  </CardLink>
+                </Card>
+              )
+            })
+          }
+          
+        </CardRow>
+      </Container>
+    </Wrapper>
   )
 }
 
